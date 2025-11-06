2ff40a9f0d41ea17c7b6c59d1713eab0726ec5d8
Priyanshu-Ku <priyanshu.kumar7500@gmail.com>
2025-11-07 01:36:29 +0530

    prog10 changed - added wdm for edgedriver

--- demo/src/main/java/com/example/prog10.java
+++ demo/src/main/java/com/example/prog10.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class prog10 {
 
@@ -38,3 +37,47 @@ public class prog10 {
         }
     }
 }
+
+
+// package com.example;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.Keys;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.edge.EdgeDriver;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+
+// public class prog10 {
+
+//     public static void main(String[] args) {
+
+//         // Correct Chrome setup
+//         WebDriverManager.chromedriver().setup();
+//         WebDriver chromeDriver = new ChromeDriver();
+//         performSearch(chromeDriver, "Chrome");
+
+//         // Correct Edge setup (requires working internet)
+//         WebDriverManager.edgedriver().setup();
+//         WebDriver edgeDriver = new EdgeDriver();
+//         performSearch(edgeDriver, "Edge");
+//     }
+
+//     public static void performSearch(WebDriver driver, String browserName) {
+//         try {
+//             driver.manage().window().maximize();
+//             driver.get("https://www.myntra.com");
+
+//             WebElement searchBox = driver.findElement(By.cssSelector("input.desktop-searchBar"));
+//             searchBox.sendKeys("Men T-Shirt");
+//             searchBox.sendKeys(Keys.ENTER);
+
+//             System.out.println(browserName + " Search Completed: " + driver.getTitle());
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         } finally {
+//             driver.quit();
+//         }
+//     }
+// }
